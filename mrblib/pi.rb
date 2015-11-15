c4872cd5ea061e21775b0d6708aa72b2d3ccc35a
sabakan21 <fujii@sein.mse.kyutech.ac.jp>
2015-11-15 16:13:05 +0900

    Add up and close methods

--- mrblib/pi.rb
+++ mrblib/pi.rb
@@ -1,35 +1,47 @@
 class Pi
   class Pin
-    attr:@pin_num
+    attr:pin_num
     def initialize(num, mode ="in")
       @pin_num = num
       File.open("/sys/class/gpio/export","w"){|f|
         f.write("#{@pin_num}")
       }
 
-      File.open("/sys/class/gpio/gpio#{pin}/direction","w"){|f|
+      File.open("/sys/class/gpio/gpio#{@pin_num}/direction","w"){|f|
         f.write(mode)
       } 
       self
     end
 
     def read
-      File.open("/sys/class/gpio/gpio#{pin}/value","r"){|f|
+      File.open("/sys/class/gpio/gpio#{@pin_num}/value","r"){|f|
         f.read
       } 
     end
 
     def write(str)
-      File.open("/sys/class/gpio/gpio#{pin}/value","w"){|f|
+      File.open("/sys/class/gpio/gpio#{@pin_num}/value","w"){|f|
         f.write(str)
       } 
     end
 
     def toggle
-      File.open("/sys/class/gpio/gpio#{pin}/value","w+"){|f|
+      File.open("/sys/class/gpio/gpio#{@pin_num}/value","w+"){|f|
         val = f.read.to_i
         f.write("#{1 - val}")
       } 
+    end
+
+    def up
+      File.open("/sys/class/gpio/gpio#{@pin_num}/direction","w"){|f|
+        f.write("high")
+      }
+    end
+
+    def close
+      File.open("/sys/class/gpio/unexport","w"){|f|
+        f.write("#{@pin_num}")
+      }
     end
   end
 
